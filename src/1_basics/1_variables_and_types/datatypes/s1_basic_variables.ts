c3946321cb1926a7cf9dd0d68af1bef6763638f9
Dileep Kumar <samudsan2020@gmail.com>
2024-12-13 06:14:50 +0530

    added array variable

---
+++ src/1_basics/1_variables_and_types/datatypes/s1_basic_variables.ts
@@ -0,0 +1,11 @@
+// String variable
+let username: string = "John";
+console.log(`Username is: ${username}`); // Outputs: Username is: Lavanya
+
+// Number variable
+let age: number = 25;
+console.log(`Age is: ${age}`); // Outputs: Age is: 25
+
+// Boolean variable
+let isLoggedIn: boolean = true;
+console.log(`Is Logged In: ${isLoggedIn}`); // Outputs: Is Logged In: true
---
+++ src/1_basics/1_variables_and_types/datatypes/s2_array_variables.ts
@@ -0,0 +1,7 @@
+// Array of strings
+let hobbies: string[] = ["Reading", "Traveling", "Coding"];
+console.log("Hobbies:", hobbies);
+
+// Array of numbers
+let scores: number[] = [10, 20, 30];
+console.log("Scores:", scores);
